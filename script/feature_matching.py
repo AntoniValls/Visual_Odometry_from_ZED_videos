22b5ee47f4c746eab8a194dbceb5d7a9e91c847b
Antoni Valls <tonivallscifre1999@gmail.com>
2025-05-21 23:36:33 +0200

    impo

--- script/feature_matching.py
+++ script/feature_matching.py
@@ -1,4 +1,6 @@
 import cv2
+import kornia as K
+import kornia.feature as KF
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -26,19 +28,29 @@ class FeatureMatcher:
         self.threshold = config['parameters']['threshold']
         self.cache_dir = f"../datasets/predicted/prefiltered_matches/{self.data_name}/{self.detector}/"
         os.makedirs(self.cache_dir, exist_ok=True)
-        
-        if self.detector != "lightglue" or "LoFTR":
-            raise ValueError("Detector must be 'lightglue' or 'LoFTR'")
-        
-        if self.detector != "LoFTR":
-            
 
         # Initialize device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        
+        # Initialize the models
+        if self.detector == "lightglue":
+            self.extractor = SuperPoint(max_num_keypoints=2048).eval().to(self.device)
+            self.matcher = LightGlue(features="superpoint").eval().to(self.device)
+        
+        elif self.detector == "LoFTR":
+            self.extractor = None
+            self.matcher =KF.LoFTR(pretrained="outdoor")
+
+        else:
+            raise ValueError("Detector must be 'lightglue' or 'LoFTR'")
+
+            
+
 
     def compute(self, image_left, next_image, idx, plot=False, show=False):
         """
-        Compute feature extraction and matching of sequential images.
+        Compute feature extraction and matching of sequential images or load
+        precomputed ones. Also plot.
 
         Args:
             image_left: first frame
@@ -49,6 +61,7 @@ class FeatureMatcher:
 
         Return:
             keypoint_left_first, keypoint_left_next, filtered_matches
+
         """
 
         cache_path = os.path.join(self.cache_dir, f"matches_{idx}.npz")
@@ -76,48 +89,67 @@ class FeatureMatcher:
             if idx == 0:
                 print(f"Computing matches and saving to {self.cache_dir}")
             
-            # LightGlue feature matching
+            # Special loadings
             sequence_dir = os.path.join(self.config['main_path'], self.config['type']) + "/"
-            image_left = load_image(sequence_dir + 'image_0/' + sorted(os.listdir(sequence_dir + 'image_0'))[idx])
-            next_image = load_image(sequence_dir + 'image_0/' + sorted(os.listdir(sequence_dir + 'image_0'))[idx + 1])
-
-            extractor = SuperPoint(max_num_keypoints=2048).eval().to(self.device)
-            matcher = LightGlue(features="superpoint").eval().to(self.device)
-
-            descriptor_left_first = extractor.extract(image_left.to(self.device))
-            descriptor_left_next = extractor.extract(next_image.to(self.device))
-            matches01 = matcher({
-             "image0": descriptor_left_first, 
-             "image1": descriptor_left_next
-             })
-
-            # Remove batch dimension
-            descriptor_left_first, descriptor_left_next, matches01 = [
-                rbd(x) for x in [descriptor_left_first, descriptor_left_next, matches01]
-            ]  
+            img_0_path = sequence_dir + 'image_0/' + sorted(os.listdir(sequence_dir + 'image_0'))[idx]
+            img_1_path = sequence_dir + 'image_0/' + sorted(os.listdir(sequence_dir + 'image_0'))[idx + 1]
             
-            # Convert to numpy arrays
-            keypoint_left_first = descriptor_left_first["keypoints"].cpu().numpy()
-            keypoint_left_next = descriptor_left_next["keypoints"].cpu().numpy()
-            matches = matches01["matches"].cpu().numpy()
-            scores = matches01["scores"].cpu().detach().numpy()
+            # LightGlue feature matching
+            if self.detector == "lightglue":
+                image_left = load_image(img_0_path)
+                next_image = load_image(img_1_path)
 
-            # Save raw data
-            np.savez(cache_path,
-                     keypoint_left_first=keypoint_left_first,
-                     keypoint_left_next=keypoint_left_next,
-                     descriptor_left_first=descriptor_left_first,
-                     descriptor_left_next=descriptor_left_next,
-                     matches=matches,
-                     scores=scores)
+                descriptor_left_first = self.extractor.extract(image_left.to(self.device))
+                descriptor_left_next = self.extractor.extract(next_image.to(self.device))
+                matches01 = self.matcher({
+                "image0": descriptor_left_first, 
+                "image1": descriptor_left_next
+                })
 
+                # Remove batch dimension
+                descriptor_left_first, descriptor_left_next, matches01 = [
+                    rbd(x) for x in [descriptor_left_first, descriptor_left_next, matches01]
+                ]  
+                
+                # Convert to numpy arrays
+                keypoint_left_first = descriptor_left_first["keypoints"].cpu().numpy()
+                keypoint_left_next = descriptor_left_next["keypoints"].cpu().numpy()
+                matches = matches01["matches"].cpu().numpy()
+                scores = matches01["scores"].cpu().detach().numpy()
 
-           # Apply top-k filtering
-            topk = np.argsort(-scores)[:self.threshold]
-            filtered_matches = matches[topk]
-            keypoint_left_first = keypoint_left_first[filtered_matches[:, 0]]
-            keypoint_left_next = keypoint_left_next[filtered_matches[:, 1]]
+                # Save raw data
+                np.savez(cache_path,
+                        keypoint_left_first=keypoint_left_first,
+                        keypoint_left_next=keypoint_left_next,
+                        descriptor_left_first=descriptor_left_first,
+                        descriptor_left_next=descriptor_left_next,
+                        matches=matches,
+                        scores=scores)
 
+                 # Apply top-k filtering
+                topk = np.argsort(-scores)[:self.threshold]
+                filtered_matches = matches[topk]
+                keypoint_left_first = keypoint_left_first[filtered_matches[:, 0]]
+                keypoint_left_next = keypoint_left_next[filtered_matches[:, 1]]
+
+            # LoFTR feature matching (using Kornia)
+            elif self.detector == "LoFTR":
+                
+                # Load image in color and convert to float32 in [0, 1]
+                image_left = K.io.load_image(img_0_path, K.io.ImageLoadType.RGB32)[None, ...]
+                next_image = K.io.load_image(img_1_path, K.io.ImageLoadType.RGB32)[None, ...]
+
+                input_dict = {"image0": K.color.rgb_to_grayscale(image_left), # LoFTR only works in grayscale
+                              "image1": K.color.rgb_to_grayscale(next_image)
+                              }
+                
+                with torch.inference_mode():        
+                    correspondences = self.matcher(input_dict)
+
+                # Convert to numpy arrays
+                keypoint_left_first = correspondences["keypoints0"].cpu().numpy()
+                keypoint_left_next = correspondences["keypoints1"].cpu().numpy()
+                
         # Plot matches every 1000 frames
         if not plot and idx % 1000 == 0:
             save_dir = f"../datasets/predicted/matches/{self.data_name}/{self.detector}_{self.threshold}"
--- script/playground.py
+++ script/playground.py
@@ -13,18 +13,18 @@ def preprocess_cv2_gray(img: np.ndarray) -> torch.Tensor:
 
 if __name__ == "__main__":
 
-    image0_np = cv2.cvtColor(cv2.imread("../datasets/BIEL/00/image_0/000200.png"), cv2.COLOR_BGR2GRAY)
-    image1_np = cv2.cvtColor(cv2.imread("../datasets/BIEL/00/image_0/000201.png"), cv2.COLOR_BGR2GRAY)
+    # Load image in color and convert to float32 in [0, 1]
+    img_0_path = "../datasets/BIEL/00/image_0/000200.png"
+    img_1_path = "../datasets/BIEL/00/image_1/000200.png"
+    
+    image_left = K.io.load_image(img_0_path, K.io.ImageLoadType.RGB32)[None, ...]
+    next_image = K.io.load_image(img_1_path, K.io.ImageLoadType.RGB32)[None, ...]
 
-     # Preprocess for LoFTR
-    image0 = preprocess_cv2_gray(image0_np)
-    image1 = preprocess_cv2_gray(image1_np)
+    input_dict = {"image0": K.color.rgb_to_grayscale(image_left), # LoFTR only works in grayscale
+                    "image1": K.color.rgb_to_grayscale(next_image)
+                    }
 
     matcher = KF.LoFTR(pretrained="outdoor")
-
-    input_dict = {"image0": image0,
-                "image1": image1
-                }
 
     with torch.inference_mode():
         correspondences = matcher(input_dict)
@@ -37,24 +37,7 @@ if __name__ == "__main__":
     Fm, inliers = cv2.findFundamentalMat(mkpts0, mkpts1, cv2.USAC_MAGSAC, 0.5, 0.999, 100000)
     inliers = inliers > 0
 
-    # Draw the matches using Kornia Moons
-    draw_LAF_matches(
-        KF.laf_from_center_scale_ori(
-            torch.from_numpy(mkpts0).view(1, -1, 2),
-            torch.ones(mkpts0.shape[0]).view(1, -1, 1, 1),
-            torch.ones(mkpts0.shape[0]).view(1, -1, 1),
-        ),
-        KF.laf_from_center_scale_ori(
-            torch.from_numpy(mkpts1).view(1, -1, 2),
-            torch.ones(mkpts1.shape[0]).view(1, -1, 1, 1),
-            torch.ones(mkpts1.shape[0]).view(1, -1, 1),
-        ),
-        torch.arange(mkpts0.shape[0]).view(-1, 1).repeat(1, 2),
-        K.tensor_to_image(image0),
-        K.tensor_to_image(image1),
-        inliers,
-        draw_dict={"inlier_color": (0.2, 1, 0.2), "tentative_color": None, "feature_color": (0.2, 0.5, 1), "vertical": False},
-    )
+    
 
 
 
